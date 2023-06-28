33caf01709e957383efd6e1cafe77b7e33377061
raul-dunca <109088288+raul-dunca@users.noreply.github.com>
2023-06-28 14:31:18 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClients = new System.Windows.Forms.DataGridView();
+            this.dgvStores = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClients
+            // 
+            this.dgvClients.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClients.Location = new System.Drawing.Point(684, 104);
+            this.dgvClients.Name = "dgvClients";
+            this.dgvClients.Size = new System.Drawing.Size(566, 319);
+            this.dgvClients.TabIndex = 0;
+            // 
+            // dgvStores
+            // 
+            this.dgvStores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStores.Location = new System.Drawing.Point(29, 104);
+            this.dgvStores.Name = "dgvStores";
+            this.dgvStores.Size = new System.Drawing.Size(566, 319);
+            this.dgvStores.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(552, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1324, 581);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvStores);
+            this.Controls.Add(this.dgvClients);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClients)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStores)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClients;
+        private System.Windows.Forms.DataGridView dgvStores;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Ex
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter dastores;
+        SqlDataAdapter daclients;
+        DataSet ds;
+        BindingSource bsstores;
+        BindingSource bsclients;
+
+        SqlCommandBuilder cmdBuilder;
+
+        string querycStores;
+        string queryClients;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daclients.Update(ds, "Client");
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); }
+        }
+        string getConnectionString()
+        {
+            return "Data Source=DESKTOP-PDRHPCB;" + "Initial Catalog=GroceyStore;Integrated Security=true";
+
+        }
+        void FillData()
+        {
+            conn = new SqlConnection(getConnectionString());
+
+            querycStores = "Select * From Store";
+            queryClients = "Select * From Client";
+
+            dastores = new SqlDataAdapter(querycStores, conn);
+            daclients = new SqlDataAdapter(queryClients, conn);
+
+            ds = new DataSet();
+
+            dastores.Fill(ds, "Store");
+            daclients.Fill(ds, "Client");
+
+
+            cmdBuilder = new SqlCommandBuilder(daclients);
+            ds.Relations.Add("Relation", ds.Tables["Store"].Columns["Sid"],
+               ds.Tables["Client"].Columns["Sid"]);
+
+
+
+            bsstores = new BindingSource();
+            bsstores.DataSource = ds.Tables["Store"];
+
+
+
+            bsclients = new BindingSource(bsstores, "Relation");
+            // bsplayers = new BindingSource(bsteams, "TeamsPlayer");
+
+
+            this.dgvStores.DataSource = bsstores;
+            this.dgvClients.DataSource = bsclients;
+
+            cmdBuilder.GetUpdateCommand();
+        }
+    }
+}
